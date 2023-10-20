0d8a1f99f5df63cf104935233869d313c22a8990
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2023-10-20 16:31:06 +0200

    new solution

--- arrays_hashing/valid_anagram.py
+++ arrays_hashing/valid_anagram.py
@@ -7,6 +7,27 @@ class Solution:
         if len(s) != len(t):
             return False
 
+        letters = {}
+        for char in s:
+            letters[char] = letters.get(char, 0) + 1
+
+        print(letters)
+
+        for char in t:
+            if char not in letters:
+                return False
+            letters[char] -= 1
+            if letters[char] == 0:
+                del letters[char]
+
+        print(letters)
+
+        return len(letters) == 0
+
+    def isAnagram2(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
         cs, ct = defaultdict(int), defaultdict(int)
         for i in range(len(s)):
             cs[s[i]] = cs[s[i]] + 1
